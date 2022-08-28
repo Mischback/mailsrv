bfd9e16ba54a36460fef552303810d0396c3dacb
Mischback <dev@mischback.de>
2022-08-28 20:47:50 +0200

    Provide a basic logging setup

--- tests/mailsrv_test_suite.py
+++ tests/mailsrv_test_suite.py
@@ -2,15 +2,44 @@
 
 # Python imports
 import argparse
+import logging
 import sys
 
+# get the general logger object
+logger = logging.getLogger(__name__)
+
 if __name__ == "__main__":
+
+    # logging setup
+    #
+    # The actual setup has to be provided only if this script is called by
+    # itsself.
+    log_handler = logging.StreamHandler(sys.stdout)
+    log_handler.setLevel(logging.DEBUG)
+    log_formatter_default = logging.Formatter("%(message)s")
+    log_formatter_debug = logging.Formatter("[%(levelname)s] - %(message)s")
+    log_handler.setFormatter(log_formatter_default)
+    logger.addHandler(log_handler)
+    logger.setLevel(logging.INFO)
 
     # setup the argument parser
     parser = argparse.ArgumentParser(
         description="Run some tests against a mail server."
     )
 
+    parser.add_argument(
+        "-v", "--verbose", help="Enable debug messages", action="store_true"
+    )
+
     args = parser.parse_args()
+
+    # enable debug messages
+    if args.verbose:
+        log_handler.setFormatter(log_formatter_debug)
+        logger.setLevel(logging.DEBUG)
+
+    logger.debug("foo")
+
+    logger.info("bar")
 
     sys.exit(0)
