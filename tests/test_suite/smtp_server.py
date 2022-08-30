e0a35b0fb80b01908ba09f18fd070ae1339e25b4
Mischback <dev@mischback.de>
2022-08-30 10:48:14 +0200

    Minor adjustments of Controller initialisation

--- tests/test_suite/smtp_server.py
+++ tests/test_suite/smtp_server.py
@@ -15,12 +15,17 @@ from aiosmtpd.handlers import Mailbox
 class SmtpServer:
     """A minimal SMTP server to be used during the test suite."""
 
-    def __init__(self):
+    def __init__(self, hostname="", port=8025, server_hostname=None):
         # Setup the SMTP server
         self.resources = contextlib.ExitStack()
         tempdir = self.resources.enter_context(tempfile.TemporaryDirectory())
         self.maildir_path = os.path.join(tempdir, "maildir")
-        self.controller = Controller(Mailbox(self.maildir_path))
+        self.controller = Controller(
+            Mailbox(self.maildir_path),
+            hostname=hostname,
+            port=port,
+            server_hostname=server_hostname,
+        )
 
         self.controller.start()
         self.resources.callback(self.controller.stop)
@@ -30,13 +35,22 @@ class SmtpServer:
 
 
 if __name__ == "__main__":
-    print("Launching a minimal SMTP server at port 8025")
+    print("Launching a minimal SMTP server at port 25")
 
     try:
-        server = SmtpServer()
+        server = SmtpServer(port=25)
         print("Server started, waiting for keyboard interrupt (Ctrl+C)")
         while True:
             time.sleep(1)
+    except PermissionError as e:
+        print("Got a PermissionError. Most likely you tried to use a privileged port.")
+        print(e)
+        print("Try using a high port (>1024) or run as root")
+        try:
+            server._stop()
+        except NameError:
+            pass
+        sys.exit(1)
     except KeyboardInterrupt:
         server._stop()
         print("Server stopped.")
