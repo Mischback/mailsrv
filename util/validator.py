9fdcfbdb658b6d8451e80699bb3adbb79286e56f
Mischback <dev@mischback.de>
2022-09-08 18:06:45 +0200

    Provide basic argparse setup

--- util/validator.py
+++ util/validator.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
+"""Run the validation suite."""
+
+
+# Python imports
+import argparse
+import sys
 
 if __name__ == "__main__":
-    print("foo")
+
+    arg_parser = argparse.ArgumentParser(
+        description="Check and validate the interdependent configuration files of the mail setup"
+    )
+
+    arg_parser.add_argument(
+        "dovecot_userdb_file", action="store", help="Dovecot's passwd-like userdb file"
+    )
+    arg_parser.add_argument(
+        "postfix_vmailbox_file", action="store", help="Postfix's virtual mailbox file"
+    )
+
+    args = arg_parser.parse_args()
+
+    print(args)
+
+    sys.exit(0)
