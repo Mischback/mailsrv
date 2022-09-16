2fca814ec852a89b29e6440cb0947a73f4669387
Mischback <dev@mischback.de>
2022-09-16 13:04:18 +0200

    Fix handling of argument list

--- util/typechecking_runner.py
+++ util/typechecking_runner.py
@@ -19,7 +19,7 @@ import sys
 if __name__ == "__main__":
     files_args = sys.argv[1:]
 
-    files = 'mypy_files="{}"'.format(" ".join(files_args))
+    files = "mypy_files={}".format(" ".join(files_args))
 
     tmp = subprocess.run(
         ["make", "local/mypy", files],
